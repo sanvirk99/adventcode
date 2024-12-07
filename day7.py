6a29a986bbdf6ca5d43ace9af39451e3b161e08c
sanvirk99 <sanseerat99@gmail.com>
2024-12-06 21:28:31 -0800

    day7

---
+++ day7.py
@@ -0,0 +1,41 @@
+
+
+# %%
+file=open('./input/day7.txt','r')
+eqs=[]
+with file as f:
+    input=file.read().strip().split('\n')
+    for eq in input:
+        total, eq = eq.split(':')
+        eq=list(map(int,eq.split()))
+        eqs.append([int(total),eq])
+# %%
+res=[]
+
+def concat(x,y):
+    val =str(x) + str(y)
+    return int(val)
+
+def dfs(sum,cur,arr,i):
+    if i == len(arr):
+        if cur == sum:
+            res.append(sum)
+            return True
+        return False
+    
+    #part two addition
+    if dfs(sum,concat(cur,arr[i]), arr, i+1):
+        return True
+
+    if dfs(sum,cur*arr[i],arr,i+1):
+        return True
+        
+    return dfs(sum,cur+arr[i],arr,i+1)
+
+for eq in eqs:
+    total,arr = eq
+    if len(arr) == 0:
+        assert False
+    dfs(total,arr[0],arr,1)
+
+print(sum(res))
