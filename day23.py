fe9c6eab9f16ed61e3b4f6ea224da1c9ab09b844
sanvirk99 <sanseerat99@gmail.com>
2024-12-23 19:19:29 -0800

    part2

--- day23.py
+++ day23.py
@@ -1,8 +1,6 @@
 import re
 from collections import defaultdict
-from itertools import combinations
-
- 
+from itertools import combinations 
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -15,13 +13,10 @@ with open('input/day23.txt','r') as f:
         graph[vs].append(vd)
         graph[vd].append(vs)
         #g.add_edge(vs,vd)
-        
 
-#nx.draw(g, with_labels = True)
+#x.draw(g, with_labels = True)
 
 def part1():
-
-   
     res=set()
     def dfs(s,hasT,comps,i):
         if i == 3:
@@ -37,15 +32,51 @@ def part1():
             arr=comps.copy()
             arr.append(neighbour)
             dfs(neighbour,hasT,arr,i+1)
-      
-
     for key in graph:
         dfs(key,False,[],0)
-    
+
     print(len(res))
     
 part1()
 
+totalV=len(graph.keys())
+def part2():
+    visited=set()
+    res=0
+    sets=defaultdict(lambda : 0)
+    stack=[]
+    def validate(s,cur,i):
+        if cur == s:
+            nonlocal res
+            sets[tuple(sorted(stack))]+=1
+            res=max(res,i)
+            return 
+        if not (i<totalV):
+            return
+        if cur in visited:
+            return
+        if not (cur in graph[s]):
+            return
+        visited.add(cur)
+        stack.append(cur)
+        for neighbour in graph[cur]:
+            validate(s,neighbour,i+1)
+        stack.pop()
+        return
+    for key in graph:
+        visited=set()
+        stack.append(key)
+        for neigbour in graph[key]:
+            validate(key,neigbour,1)
+        stack.pop()
+    maxkey=None
+    maxvalue=0
+    for key,value in sets.items():
+        if len(key) == value:
+            if value > maxvalue:
+                maxkey=key
+                maxvalue=value
+    
+    print(','.join(maxkey))
 
-
-#part2()
+part2()
